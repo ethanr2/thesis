b86cc03e53cad569c56dd42021a17ca34af90a0b
Ethan Rahman <ethanrhmn@gmail.com>
2022-01-03 19:25:03 -0600

    Figured out I forgot a step in the data alignment process. Need to align the quarters consistently for the difference terms in the regression

--- exploratory.py
+++ exploratory.py
@@ -8,6 +8,9 @@ import numpy as np
 import pandas as pd
 from scipy import stats
 
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
 from bokeh.io import export_png, output_file, show
 from bokeh.plotting import figure
 from bokeh.models import NumeralTickFormatter, LabelSet, ColumnDataSource
@@ -15,10 +18,11 @@ from bokeh.models.tickers import FixedTicker
 from bokeh.layouts import row, column
 
 
-TRUNC = (dt(1969, 2, 25), dt(1997, 1, 1))
+TRUNC = (dt(1969, 2, 1), dt(1996, 1, 1))
 COLS = {
     "gRGDP": ("gRGDPB1", "gRGDPF0", "gRGDPF1", "gRGDPF2"),
     "gPGDP": ("gPGDPB1", "gPGDPF0", "gPGDPF1", "gPGDPF2"),
+    "UNEMP": ("UNEMPF0"),
 }
 
 # Takes RGDP forecasts from the Greenbook dataset
@@ -29,13 +33,16 @@ def get_var(var):
     dates = df.pop("GBdate")
 
     df = df.loc[(TRUNC[0] < dates) & (dates < TRUNC[1]), COLS[var]]
-    df = df.loc[~df[var + "F2"].isnull(), :]
+    # df = df.loc[~df[var + "F2"].isnull(), :]
     return df
 
 
 # Takes the intended FFR rates data directly from Romer and Romer's dataset
-def get_intended_rates():
-    int_rate = pd.read_excel("data/RomerandRomerDataAppendix.xls", usecols=[0, 1, 2])
+def get_intended_rates(drop_cols = True):
+    if drop_cols:
+        int_rate = pd.read_excel("data/RomerandRomerDataAppendix.xls", usecols=[0, 1, 2])
+    else:
+        int_rate = pd.read_excel("data/RomerandRomerDataAppendix.xls")
     int_rate["MTGDATE"] = int_rate["MTGDATE"].astype(str)
 
     # Weird parsing glitch because the dates were not zero padded. Might have cleaner fix.
@@ -52,41 +59,64 @@ def get_intended_rates():
 
 # The GB forecasts aren't released at the same time as the meetings so this function makes sure
 # that each row from the interest rate data is aligned with its proper row in the GB dataset
-def align_dates(rgdp, int_rate):
+def align_dates(df, int_rate):
     ir_date = int_rate.pop("mtg_date")
-    rgdp_date = rgdp.index.to_series()
+    df_date = df.index.to_series()
 
     # Corresponding MTG date associated with greenbook forecast
-    rgdp["mtg_date"] = pd.NaT
+    df["mtg_date"] = pd.NaT
 
     for date in ir_date:
-        i = rgdp_date.searchsorted(date) - 1
-        if i == 269:
-            break
-        rgdp.iloc[i, 4] = date
+        i = df_date.searchsorted(date) - 1
+        # if i == 269:
+        #     break
+        df.loc[df_date[i], "mtg_date"] = date
 
-        print("ir_date: {}, rgdp_Date: {}".format(date, rgdp_date.iloc[i]))
-    rgdp = rgdp.reset_index().set_index("mtg_date")
-    rgdp["ffr"] = int_rate["OLDTARG"]
+        print("ir_date: {}, GB_Date: {}".format(date, df_date.iloc[i]))
+    df = df.reset_index().set_index("mtg_date")
+    df["ffr"] = int_rate["OLDTARG"]
+    df["diff_ffr"] = int_rate["DTARG"]
+    return df
 
-    return rgdp
 
-
-rgdp = get_var("gRGDP")
-df = get_var("gPGDP")
-rgdp = rgdp.join(df)
-rgdp
-#%%
+raw_data = get_var("gRGDP").join(get_var("gPGDP")).join(get_var("UNEMP"))
 int_rate = get_intended_rates()
-df = align_dates(rgdp, int_rate)
-# for whatever reason there appears to be missing data in romer and romer's dataset for 1979-10-12? just dropping it for now.
+raw_data = align_dates(raw_data, int_rate)
+
+# For whatever reason there appears to be missing data in romer and romer's dataset for 1979-10-12? 
+# just dropping it for now.
+# Missing GB data on mtg_date 1971-08-24, GB_date 1971-8-20
+raw_data = raw_data.dropna()
+print(raw_data)
+#%%
+df = raw_data.copy()
+for col in COLS["gRGDP"]:
+    df["diff_" + col] = df[col].diff(1)
+for col in COLS["gPGDP"]:
+    df["diff_" + col] = df[col].diff(1)
+
 df = df.dropna()
+
+model = smf.ols(("diff_ffr ~ ffr + gRGDPB1 + gRGDPF0 + gRGDPF1 + gRGDPF2"
+                 "+ diff_gRGDPB1 + diff_gRGDPF0 + diff_gRGDPF1 + diff_gRGDPF2" 
+                 "+ gPGDPB1 + gPGDPF0 + gPGDPF1 + gPGDPF2"
+                 "+ diff_gPGDPB1 + diff_gPGDPF0 + diff_gPGDPF1 + diff_gPGDPF2"
+                 "+ UNEMPF0"),
+                data=df
+                )
+
+
+res = model.fit()
+res.summary()
+
 #%%
 
+# We need to realign the quarters for the diff terms...
 
-#%%
-
-
+romer_df = get_intended_rates(False)
+temp = pd.concat([romer_df["IGRDM"], df["diff_gPGDPB1"]], axis = 1)
+temp = pd.concat([romer_df["IGRDM"], df["diff_gPGDPB1"]], axis = 1)
+temp
 #%%
 def set_up(x, y, truncated=True, margins=None):
     if truncated:
