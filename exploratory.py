2dedcd31b1997f8dacb62a491280698692010275
Ethan Rahman <ethanrhmn@gmail.com>
2022-03-14 21:26:19 -0500

    The research idea has gotten more specific, I probably wont need to extend the romer and romer sample anymore but I may want to at a later date. I realized the replicaiton attempt is actually pretty accurate anyway so theres no need for conern.

--- exploratory.py
+++ exploratory.py
@@ -11,11 +11,7 @@ from scipy import stats
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
-from bokeh.io import export_png, output_file, show
-from bokeh.plotting import figure
-from bokeh.models import NumeralTickFormatter, LabelSet, ColumnDataSource
-from bokeh.models.tickers import FixedTicker
-from bokeh.layouts import row, column
+
 
 
 TRUNC = (dt(1969, 1, 1), dt(1997, 1, 1))
@@ -64,7 +60,7 @@ def get_intended_rates(drop_cols=True):
 def align_dates(df, int_rate):
     ir_date = int_rate.pop("mtg_date")
     df_date = df.index.to_series()
-    #return df_date
+    # return df_date
     # Corresponding MTG date associated with greenbook forecast
     df["mtg_date"] = pd.NaT
 
@@ -72,58 +68,18 @@ def align_dates(df, int_rate):
         i = df_date.searchsorted(date) - 1
         # if i == 269:
         #     break
-        # if df_date[i] == dt(year = 1979, month = 9, day = 12):
-        #         continue
+        if df_date[i] == dt(year = 1979, month = 9, day = 12):
+             #continue
+             print(i, "ir_date: {}, GB_Date: {}".format(date, df_date.iloc[i]))
         df.loc[df_date[i], "mtg_date"] = date
-        print(i, "ir_date: {}, GB_Date: {}".format(date, df_date.iloc[i]))
-        
-    df = df.loc[~df["mtg_date"].isna(),:]    
+        #print(i, "ir_date: {}, GB_Date: {}".format(date, df_date.iloc[i]))
+
+    df = df.loc[~df["mtg_date"].isna(), :]
     df = df.reset_index().set_index(["mtg_date"])
     df["ffr"] = int_rate["OLDTARG"]
     df["diff_ffr"] = int_rate["DTARG"]
     return df
 
-# We need to realign the quarters for the diff terms...
-def calc_diffs(raw_data):
-    stacked = raw_data.drop(["GBdate", "ffr", "diff_ffr", "UNEMPF0"], axis=1)
-    stacked = stacked.stack().reset_index()
-    stacked["var"] = stacked["level_1"].apply(lambda x: x[:-2])
-    
-    REL_QUARTER_DICT = {"B2": -2, "B1": -1, "F0": 0, "F1": 1, "F2": 2, "F3": 3}
-    stacked["rel_quarter"] = stacked["level_1"].apply(lambda x: REL_QUARTER_DICT[x[-2:]])
-    stacked = stacked.drop("level_1", axis=1)
-    
-    get_abs_quarter= lambda x: (x.month - 1)//3 + 1 + 4 * (x.year - TRUNC[0].year)
-    stacked["abs_quarter"] = stacked["mtg_date"].apply(get_abs_quarter) + stacked["rel_quarter"]
-    
-    
-    groups = stacked.groupby("mtg_date")
-    mtg_dates = raw_data.index.tolist()
-    
-    diffs = {}
-    
-    for m in range(1, len(mtg_dates)):
-        #print(m, mtg_dates[m])
-        group = groups.get_group(mtg_dates[m])
-        last_group = groups.get_group(mtg_dates[m-1])
-        
-        for i in range(-1,3):
-            temp = group.loc[group["rel_quarter"] == i,:].set_index("var")
-            if not temp.any().any():
-                continue
-            j = temp["abs_quarter"].iloc[0]
-            temp_last = last_group.loc[last_group["abs_quarter"] == j,:].set_index("var")
-            
-            diffs[(mtg_dates[m], i)] =  round(temp[0] - temp_last[0], 1)
-    
-    diffs = pd.DataFrame(diffs).T.stack().reset_index()
-    
-    diffs["var"] = "diff_" + diffs["var"] + diffs["level_1"].astype(str)
-    diffs["var"] = diffs["var"].apply(lambda x: x.replace("-1", "M"))
-    diffs["mtg_date"] = diffs["level_0"]
-    diffs = diffs.drop(["level_0","level_1"], axis = 1)
-    
-    return pd.pivot_table(diffs, values = 0, index = "mtg_date", columns = "var")
 
 raw_data = get_var("gRGDP").join(get_var("gPGDP")).join(get_var("UNEMP"))
 
@@ -133,9 +89,93 @@ raw_data = align_dates(raw_data, int_rate)
 # For whatever reason there appears to be missing data in romer and romer's dataset for 1979-10-12?
 # just dropping it for now.
 # Missing GB data on mtg_date 1971-08-24, GB_date 1971-8-20
+#%%
+# df = raw_data.copy()
+# ir_date = int_rate["mtg_date"]
+# df_date = df.index.to_series()
 
+# # Corresponding MTG date associated with greenbook forecast
+# int_rate["GBdate"] = pd.NaT
+
+# for date in df_date:
+#     i = ir_date.searchsorted(date)
+#     # if i == 269:
+#     #     break
+#     #if df_date[i] == dt(year = 1979, month = 9, day = 12):
+#          #continue
+#     #     print(i, "ir_date: {}, GB_Date: {}".format(date, df_date.iloc[i]))
+#     #df.loc[df_date[i], "mtg_date"] = date
+#     print(i, "ir_date: {}, GB_Date: {}".format(ir_date[i], date))
+
+# # df = df.loc[~df["mtg_date"].isna(), :]
+# # df = df.reset_index().set_index(["mtg_date"])
+# # df["ffr"] = int_rate["OLDTARG"]
+# # df["diff_ffr"] = int_rate["DTARG"]
+
+
+#%%
+
+# We need to realign the quarters for the diff terms...
+def calc_diffs(raw_data):
+    stacked = (
+        raw_data.drop(["ffr", "diff_ffr", "UNEMPF0"], axis=1)
+        .reset_index()
+        .set_index(["mtg_date", "GBdate"])
+        .stack()
+        .reset_index()
+    )
+    
+    stacked["var"] = stacked["level_2"].apply(lambda x: x[:-2])
+    REL_QUARTER_DICT = {"B2": -2, "B1": -1, "F0": 0, "F1": 1, "F2": 2, "F3": 3}
+    stacked["rel_quarter"] = stacked["level_2"].apply(lambda x: REL_QUARTER_DICT[x[-2:]])
+    
+    get_abs_quarter = lambda x: (x.month - 1) // 3 + 1 + 4 * (x.year - TRUNC[0].year)
+    stacked["abs_quarter_mtg"] = (
+        stacked["mtg_date"].apply(get_abs_quarter) + stacked["rel_quarter"]
+    )
+    stacked["abs_quarter_GB"] = (
+        stacked["GBdate"].apply(get_abs_quarter) + stacked["rel_quarter"]
+    )
+    
+    groups = stacked.groupby("mtg_date")
+    mtg_dates = raw_data.index.tolist()
+    
+    diffs = {}
+    
+    for m in range(1, len(mtg_dates)):
+        # print(m, mtg_dates[m])
+        group = groups.get_group(mtg_dates[m])
+        last_group = groups.get_group(mtg_dates[m - 1])
+    
+        for i in range(-1, 3):
+            temp = group.loc[group["rel_quarter"] == i, :].set_index("var")
+            if not temp.any().any():
+                continue
+            j = temp["abs_quarter_GB"].iloc[0]
+            temp_last = last_group.loc[last_group["abs_quarter_GB"] == j, :].set_index("var")
+    
+            diffs[(mtg_dates[m], i)] = round(temp[0] - temp_last[0], 4)
+            calc = pd.DataFrame(
+                {"diff": diffs[(mtg_dates[m], i)], "new": temp[0], "old": temp_last[0]}
+            )
+            # print(calc)
+    
+    diffs = pd.DataFrame(diffs).T.stack().reset_index()
+    
+    diffs["var"] = "diff_" + diffs["var"] + diffs["level_1"].astype(str)
+    diffs["var"] = diffs["var"].apply(lambda x: x.replace("-1", "M"))
+    diffs["mtg_date"] = diffs["level_0"]
+    diffs = diffs.drop(["level_0", "level_1"], axis=1)
+    
+    diffs = pd.pivot_table(diffs, values=0, index="mtg_date", columns="var")
+    
+    return diffs
 diffs = calc_diffs(raw_data)
-data = raw_data.merge(diffs, how = "left", on= "mtg_date").drop(["gRGDPB2", "gRGDPF3", "gPGDPB2", "gPGDPF3"], axis = 1).dropna()
+data = (
+    raw_data.merge(diffs, how="left", on="mtg_date")
+    .drop(["gRGDPB2", "gRGDPF3", "gPGDPB2", "gPGDPF3"], axis=1)
+    #.dropna()
+)
 print(data)
 #%%
 df = data.copy()
@@ -152,23 +192,19 @@ model = smf.ols(
     data=df,
 )
 
-
 res = model.fit()
 res.summary()
 
-#%%
-romer_data = get_intended_rates(False)
-romer_data["rep_shock"] = res.resid
-residuals = romer_data.loc[:, ["RESIDF", "rep_shock"]].dropna()
 
-romer_data["diff_gPGDPM"] = data["diff_gPGDPM"]
-inf_now = romer_data.loc[:, ["IGRDM", "diff_gPGDPM"]].dropna()
 
-corrected_inf = inf_now["IGRDM"] == inf_now["diff_gPGDPM"]
-for i, row in inf_now.iterrows():
-    print(row["IGRDM"], row["diff_gPGDPM"], row["IGRDM"] == row["diff_gPGDPM"])
-corrected_inf.sum()/ corrected_inf.size
 #%%
+
+from bokeh.io import export_png, output_file, show
+from bokeh.plotting import figure
+from bokeh.models import NumeralTickFormatter, LabelSet, ColumnDataSource
+from bokeh.models.tickers import FixedTicker
+from bokeh.layouts import row, column
+
 def set_up(x, y, truncated=True, margins=None):
     if truncated:
         b = (3 * y.min() - y.max()) / 2
@@ -245,14 +281,17 @@ def chart1(df):
 
 # Chart of a regression e.g. inflation vs money supply
 def chart2(df):
-    xdata, ydata, xrng, yrng = set_up(df.iloc[:,0]/100, df.iloc[:,1]/100, truncated=False, margins=0.005)
+    df = df.dropna()
+    xdata, ydata, xrng, yrng = set_up(
+        df.iloc[:, 0] / 100, df.iloc[:, 1] / 100, truncated=False, margins=0.005
+    )
 
     p = figure(
         width=500,
         height=500,
-        title="Reproduction Attempt",
-        x_axis_label="Romer and Romer IGRDM",
-        y_axis_label="Reproduced IGRDM",
+        title="Reproduction Attempt: " + df.columns[0],
+        x_axis_label="Romer and Romer " + df.columns[0],
+        y_axis_label="Reproduced " + df.columns[0],
         y_range=yrng,
         x_range=xrng,
     )
@@ -260,8 +299,8 @@ def chart2(df):
     p.line([0, 0], yrng, color="black")
 
     slope, intercept, r_value, p_value, std_err = stats.linregress(xdata, ydata)
-    leg = "R = {:.4f}, P-Value = {:.4e}, Slope = {:.4f}".format(r_value, p_value, slope)
-    p.line(xdata, xdata * slope + intercept, legend=leg, color="black")
+    leg = "R = {:.4f}, Slope = {:.4f}".format(r_value, slope)
+    p.line(xdata, xdata * slope + intercept, legend_label=leg, color="black")
     p.circle(xdata, ydata, color="blue", size=2)
 
     p.xaxis[0].ticker.desired_num_ticks = 10
@@ -269,9 +308,94 @@ def chart2(df):
     p.ygrid.grid_line_color = None
     p.xaxis.formatter = NumeralTickFormatter(format="0.0%")
     p.yaxis.formatter = NumeralTickFormatter(format="0.0%")
-
+    p.legend.location = "bottom_right"
+    
     export_png(p, filename="imgs/chart2.png")
 
     return p
+ROMER_REP_DICT = {
+    "diff_ffr":"DTARG",
+    "ffr":"OLDTARG",
+    "gRGDPB1":"GRAYM",
+    "gRGDPF0":"GRAY0", 
+    "gRGDPF1":"GRAY1", 
+    "gRGDPF2":"GRAY2",
+    "diff_gRGDPM":"IGRYM",
+    "diff_gRGDP0":"IGRY0",
+    "diff_gRGDP1":"IGRY1", 
+    "diff_gRGDP2":"IGRY2",
+    "gPGDPB1":"GRADM",
+    "gPGDPF0":"GRAD0",
+    "gPGDPF1":"GRAD1",
+    "gPGDPF2":"GRAD2",
+    "diff_gPGDPM":"IGRDM", 
+    "diff_gPGDP0":"IGRD0", 
+    "diff_gPGDP1":"IGRD1", 
+    "diff_gPGDP2":"IGRD2",
+    "UNEMPF0":"GRAU0",
+    "rep_shock":"RESID"
+}
+romer_data = get_intended_rates(False)
+data["rep_shock"] = res.resid
 
-show(chart2(inf_now))
+#data= data.rename(ROMER_REP_DICT, axis = 1)
+
+#data = data.drop("GBdate", axis = 1) == romer_data.drop("mtg_date", axis = 1)
+#data = data.loc[romer_data.index,:]
+#%%
+
+# This draws plots comparing my dataset with romer and romers. 
+plots = []
+
+
+for key, value in ROMER_REP_DICT.items():
+    temp = pd.concat([romer_data[value], data[key]], axis = 1).dropna()
+    plots.append(chart2(temp))
+    
+    accuracy = sum(temp[key] == temp[value])/temp[key].size
+    SSR = sum((temp[key] - temp[value])**2)
+    
+    print(key, value, temp[key].size, accuracy, SSR)
+
+show(
+     column(
+         row(plots[0], plots[1], plots[2]),
+         row(plots[3], plots[4], plots[5]),
+         row(plots[6], plots[7], plots[8]),
+         row(plots[9], plots[10], plots[11]),
+         row(plots[12], plots[13], plots[14]),
+         row(plots[15], plots[16], plots[17]),
+         row(plots[18], plots[19])
+         )
+     )
+
+# Kinda stuck. current plans:
+# 1. Replicate the indicator from Romer and Romer's data directly.
+# 2. Set up a method to determine percisely witch observations are different from RR
+# 3. Investigate those observation to get a hint at whats going on.
+
+#%%
+# Step one, verify from Romer's own dataset. 
+
+model = smf.ols(
+    (
+        "DTARG ~ OLDTARG"
+        "+ GRAYM + GRAY0 + GRAY1 + GRAY2"
+        "+ IGRYM + IGRY0 + IGRY1 + IGRY2"
+        "+ GRADM + GRAD0 + GRAD1 + GRAD2"
+        "+ IGRDM + IGRD0 + IGRD1 + IGRD2"
+        "+ GRAU0"
+    ),
+    data=romer_data.dropna(),
+)
+
+res = model.fit()
+res.summary()
+temp = pd.concat([romer_data['RESID'], res.resid], axis = 1).dropna()
+print(res.resid)
+print(temp)
+show(chart2(temp))
+
+#%%
+# Step two
+
