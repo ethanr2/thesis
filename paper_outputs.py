e272475904e36deabc92b7c54970310cd8363516
Ethan Rahman <ethanrhmn@gmail.com>
2022-05-06 05:08:48 -0500

    Just realized I cited the wrong paper for the policy shock time series in my introduction. Going to delete some files next commit.

--- paper_outputs.py
+++ paper_outputs.py
@@ -26,7 +26,87 @@ from bokeh.layouts import row, column
 from stargazer.stargazer import Stargazer
 
 TAB_PATH = "thesis_draft/latex_tables/"
+CHART_PATH = "thesis_draft/charts/"
 
+NIUred = (200, 16, 46)
+NIUpantone = (165, 167, 168)
+#%%
+# Introduction
+def set_up(x, y, truncated=True, margins=None):
+    if truncated:
+        b = (3 * y.min() - y.max()) / 2
+    else:
+        b = y.min()
+    if margins == None:
+        xrng = (x.min(), x.max())
+        yrng = (b, y.max())
+    else:
+        xrng = (x.min() - margins, x.max() + margins)
+        yrng = (b - margins, y.max() + margins)
+
+    x = x.dropna()
+    y = y.dropna()
+
+    return (x, y, xrng, yrng)
+
+
+def intro_charts(df, series, title, name):
+    xdata, ydata, xrng, yrng = set_up(df.index, df[series], truncated=False)
+    scale = 1
+    p = figure(
+        width=int(1000 * scale),
+        height=int(666 * scale),
+        title=title,
+        x_axis_label="Date",
+        x_axis_type="datetime",
+        y_range=yrng,
+        x_range=xrng,
+        toolbar_location=None,
+    )
+    p.line(xrng, [0, 0], color="black", width=1)
+
+    p.line(xdata, ydata, color=NIUred, width=2)
+
+    p.xaxis[0].ticker.desired_num_ticks = 10
+    # p.legend.location = "bottom_right"
+    p.xgrid.grid_line_color = None
+    p.ygrid.grid_line_color = None
+    p.yaxis.formatter = NumeralTickFormatter(format="0.0%")
+    # p.xaxis.major_label_orientation = math.pi/4
+    p.title.text_font_size = "16pt"
+    p.xaxis.axis_label_text_font_size = "14pt"
+    p.yaxis.axis_label_text_font_size = "14pt"
+    # p.legend.label_text_font_size = "14pt"
+    export_png(p, filename=CHART_PATH + name)
+
+    return p
+
+
+ns_df = (
+    pd.read_excel(
+        "data/NS_95Sample.xlsx",
+        sheet_name="PolicyNewsShocks1995",
+        index_col="date_daily",
+        parse_dates=True,
+    ).dropna()
+    / 100
+)
+
+rr_df = pd.read_excel("data/RomerandRomerDataAppendix.xls").dropna()
+date_convert = lambda x: dt(
+    year=int(x[-2:]) + 1900, day=int(x[-4:-2]), month=int(x[:-4])
+)
+rr_df.index = rr_df["MTGDATE"].astype(str).apply(date_convert)
+rr_df = rr_df / 100
+show(
+    column(
+        intro_charts(
+            ns_df, "FFR_shock", "Nakamura and Steinsson Baseline Indicator", "ns15.png"
+        ),
+        intro_charts(rr_df, "RESIDF", "Romer and Romer Indicator", "rr04.png"),
+    )
+)
+#%%
 full_sample = pd.read_pickle("data/processed_data/full_sample_ns_data.pkl")
 coefs = pd.read_pickle("data/processed_data/bootstrap.pkl")
 model = smf.ols(
